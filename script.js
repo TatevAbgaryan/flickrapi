92062751b7829d572d5f1a0a9d1072ee4365865b
Tatev <tatev@abgaryan.net>
2014-08-15 00:21:50 +0400

    save 5 items in txt file and show them on request

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 (function($) {
 	var form = document.getElementsByTagName('form')[0],
 		searchField = form.search,
-	    input = document.getElementsByTagName('input')[0];
+	    input = document.getElementsByTagName('input')[0],
+	    timer,
+	    tosend = "";
 
 // o.O input != $("#searchInput") ????
 	
 	$("#searchInput").keyup(function(){
-		setTimeout( function(){searchFlickr(searchField.value)}, 10000);
+		clearTimeout(timer);
+		timer = setTimeout( function(){searchFlickr(searchField.value)}, 3000);
 		//alert( input.value );
 
 	});
 	form.onsubmit = function(e) {
+		clearTimeout(timer);
 		searchFlickr(searchField.value);
 		return false;
 	};
@@ -28,16 +32,36 @@
 			success: function(data) {
 				
 				var temp=data;
-				document.getElementsByTagName('ul')[0].innerHTML= "<li><img src=\""+temp.items[0].media.m+"\" alt /></li><li>"+temp.items[0].author+"</li><li>"+temp.items[0].title+"</li>";
+				console.log(data);
+				for(var i = 0; i<5; ++i){
+					var towrite = document.getElementsByTagName('ul')[0].innerHTML += "<li><img src=\""+temp.items[i].media.m+"\" alt /></li>";
+					towrite = '<!DOCTYPE html><html><body><ul>' + towrite + '</ul></body></html>';
+				};
+				for(var i = 0; i<5; ++i){
+					tosend += temp.items[i].media.m + "\n";
+				};
 
+				$.ajax({
+					url: 'http://127.0.0.1:1337/request',
+					//data: tosend,
+					data: {
+						first:tosend,
+						second:towrite
+					},
+					type: 'POST',
+					jsonpCallback: 'callback', // this is not relevant to the POST anymore
+					success: function (data) {
+						console.log('Success: ')
+					},
+					error: function (xhr, status, error) {
+						console.log('Error: ' + error.message);
+					},
+				});
 			},
 			error: function(data) {
 				alert(JSON.stringify(data));
 			}
 		});
+
 	}
-
-
-
-
 })(jQuery);
